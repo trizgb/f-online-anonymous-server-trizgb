815bda895ec916e3bc284270186d4f748f0696c3
Beatriz Gomez <beatrizgomez92@gmail.com>
2019-03-31 21:32:47 +0200

    Footer margin fixed and menu button hidden when its clicked

--- js/main.js
+++ js/main.js
@@ -7,11 +7,13 @@ const menu = document.querySelector('.nav');
 function showMenu() {
   menu.classList.add('show');
   menu.classList.remove('hide');
+  openBtn.classList.toggle('hide');
 }
 
 function hideMenu() {
   menu.classList.add('hide');
   menu.classList.remove('show');
+  openBtn.classList.remove('hide');
 }
 
 openBtn.addEventListener('click', showMenu);
