2af0af29284467f2181c766e754cb9df821aa90d
Beatriz Gomez <beatrizgomez92@gmail.com>
2019-03-31 21:15:58 +0200

    Hover effects in buttons and links

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# clarke-fc-anonymous-server
+# Anonymous Server
 Base para la prueba técnica 4 de la formación continua en Adalab
--- js/main.js
+++ js/main.js
@@ -5,13 +5,11 @@ const closeBtn = document.querySelector('.nav-close');
 const menu = document.querySelector('.nav');
 
 function showMenu() {
-  console.log('open');
   menu.classList.add('show');
   menu.classList.remove('hide');
 }
 
 function hideMenu() {
-  console.log('hide');
   menu.classList.add('hide');
   menu.classList.remove('show');
 }
